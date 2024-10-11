78c0c0aa13487cf62afda799306d9703fbb55c53
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-10-11 17:12:47 +0530

    learnt datatype in javascript

--- Javascript/script.js
+++ Javascript/script.js
@@ -18,3 +18,19 @@ console.log(fullName);
 const number = 29;
 console.log(number);
 
+//DataTpes
+let name = "harsh" //string
+let number1 = 31 //int
+let sach = true //bool
+
+console.log(typeof number)
+
+// objects
+const student = {
+    fullName1 : "Harsh",
+    age1 : 20,
+    cgpa1 : 8.3
+}
+console.log(student.fullName1)
+
+student["fullName1"]="Harsh Vardhan Upadhyay";
