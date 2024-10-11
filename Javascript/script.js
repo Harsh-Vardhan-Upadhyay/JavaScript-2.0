f2db75d93b00cbc96e0d804f3d557f12666fbd64
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-10-11 16:43:10 +0530

    learnt about variables

---
+++ Javascript/script.js
@@ -0,0 +1,20 @@
+//<-----------------------Variables----------------------->
+console.log("namaste duniya");
+console.log("namaste duniya");
+console.log("namaste duniya");
+
+
+//let 
+let fullName = "Harsh Vardhan";
+console.log(fullName);
+
+//Update fullName variable 
+
+fullName = "Harsh Vardhan Upadhyay";
+console.log(fullName);
+
+//const
+
+const number = 29;
+console.log(number);
+
