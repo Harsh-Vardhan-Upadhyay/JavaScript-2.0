05cf5282a27713e3aaf7eed74a206f5b7453c5f4
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-12-27 00:31:36 +0530

    :zap: Solved few practice questions

--- Javascript/script.js
+++ Javascript/script.js
@@ -72,3 +72,34 @@ let category = age1 >= 18 ? "adult" : "minor";
 console.log(`The result is ${category}`);
 
 
+//<----------------------- Practice Questions ------------------------->
+// Get input from uesr and check if the number is multiple of 5 
+
+let Number = prompt("Enter your Number")
+if(Number % 5 == 0){
+    console.log(`The number ${Number} is multiple of 5`)
+}
+else{
+    console.log(`The number ${Number} is not multiple of 5`)
+}
+
+// Qs2. Write a code which can give grades to students according to their scores:
+
+let Grade = prompt("Enter your grade");
+Grade = Number(Grade);
+
+if (Grade < 0 || Grade > 100) {
+    alert("Invalid entry");
+} else if (Grade >= 90 && Grade <= 100) {
+    console.log("You have scored A Grade");
+} else if (Grade >= 80 && Grade <= 89) {
+    console.log("You have scored B Grade");
+} else if (Grade >= 70 && Grade <= 79) {
+    console.log("You have scored C Grade");
+} else if (Grade >= 60 && Grade <= 69) {
+    console.log("You have scored D Grade");
+} else if (Grade >= 50 && Grade <= 59) {
+    console.log("You have scored E Grade");
+} else {
+    console.log("You have scored F Grade");
+}
